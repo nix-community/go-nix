d1bdb3a0d0d8c77a184f473d32e8b43b92a7610e
Florian Klink <flokli@flokli.de>
2021-02-21 19:19:35 +0100

    list_test: remove Println, add proper assert.Equal

--- src/nar/list_test.go
+++ src/nar/list_test.go
@@ -1,7 +1,6 @@
 package nar_test
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 
@@ -35,5 +34,25 @@ func TestLS(t *testing.T) {
 	r := strings.NewReader(fixture)
 	root, err := nar.ParseLS(r)
 	assert.NoError(t, err)
-	fmt.Println(root)
+
+	expected_root := &nar.LSRoot{
+		Version: 1,
+		Root: nar.LSEntry{
+			Type: nar.TypeDirectory,
+			Entries: map[string]nar.LSEntry{
+				"bin": nar.LSEntry{
+					Type: nar.TypeDirectory,
+					Entries: map[string]nar.LSEntry{
+						"curl": nar.LSEntry{
+							Type:       nar.TypeRegular,
+							Size:       182520,
+							Executable: true,
+							NAROffset:  400,
+						},
+					},
+				},
+			},
+		},
+	}
+	assert.Equal(t, expected_root, root)
 }
