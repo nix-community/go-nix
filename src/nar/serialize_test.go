16a612d8df6fff8bec224470099e9fc2db458a5d
Florian Klink <flokli@flokli.de>
2021-02-21 18:48:45 +0100

    nar/serialize_test.go: add failing test

--- src/nar/serialize_test.go
+++ src/nar/serialize_test.go
@@ -15,5 +15,18 @@ func TestReadLongLong(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, num, int64(13))
+}
 
+// TestReadLongLongPartial constructs a reader not returning the full
+// string on the first Read() call
+func TestReadLongLongPartial(t *testing.T) {
+	r := &hesitantReader{data: [][]byte{
+		{13},
+		{},
+		{0, 0, 0, 0, 0, 0, 0},
+	}}
+
+	num, err := readLongLong(r)
+	assert.NoError(t, err)
+	assert.Equal(t, num, int64(13))
 }
