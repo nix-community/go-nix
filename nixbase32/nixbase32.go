890ad15e21d2b705410d9129f8e43d17b947ae93
Florian Klink <flokli@flokli.de>
2021-02-26 19:08:35 +0100

    nixbase32: change signatures to match these of encoding/base32.Encoding

--- nixbase32/nixbase32.go
+++ nixbase32/nixbase32.go
@@ -1,48 +1,59 @@
 package nixbase32
 
 import (
-	"encoding/base32"
+	"fmt"
 	"strings"
 )
 
 const alphabet = "0123456789abcdfghijklmnpqrsvwxyz"
 
+// EncodedLen returns the length in bytes of the base32 encoding of an input
+// buffer of length n.
 func EncodedLen(n int) int {
 	return (n*8-1)/5 + 1
 }
 
-func Encode(buf []byte) string {
-	l := EncodedLen(len(buf))
+// DecodedLen returns the maximum length in bytes of the decoded data
+// corresponding to n bytes of base32-encoded data.
+func DecodedLen(n int) int {
+	return (n * 5) / 8
+}
+
+// EncodeToString returns the nixbase32 encoding of src.
+func EncodeToString(src []byte) string {
+	l := EncodedLen(len(src))
 	s := ""
 	for n := l - 1; n >= 0; n-- {
 		b := uint(n * 5)
 		i := uint(b / 8)
 		j := uint(b % 8)
-		c := buf[i] >> j
-		if i+1 < uint(len(buf)) {
-			c |= buf[i+1] << (8 - j)
+		c := src[i] >> j
+		if i+1 < uint(len(src)) {
+			c |= src[i+1] << (8 - j)
 		}
 		s += string(alphabet[c&0x1f])
 	}
 	return s
 }
 
-func Decode(buf []byte, s string) (ok bool) {
+// DecodeString returns the bytes represented by the nixbase32 string s.
+func DecodeString(s string) ([]byte, error) {
+	dst := make([]byte, DecodedLen(len(s)))
 	for n := 0; n < len(s); n++ {
 		c := s[len(s)-n-1]
 		digit := strings.IndexByte(alphabet, c)
 		if digit == -1 {
-			return
+			return nil, fmt.Errorf("character %v not in alphabet!", c)
 		}
 		b := uint(n * 5)
 		i := uint(b / 8)
 		j := uint(b % 8)
-		buf[i] |= byte(digit) << j
-		if i+1 < uint(len(buf)) {
-			buf[i+1] |= byte(digit) >> (8 - j)
+		dst[i] |= byte(digit) << j
+		if i+1 < uint(len(dst)) {
+			dst[i+1] |= byte(digit) >> (8 - j)
 		} else if digit>>(8-j) != 0 {
-			return
+			return nil, fmt.Errorf("TODO")
 		}
 	}
-	return true
+	return dst, nil
 }
