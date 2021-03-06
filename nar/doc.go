3301f555a846059241f512329d9cfc4449008b6e
Florian Klink <flokli@flokli.de>
2021-03-06 13:14:54 +0100

    nar/doc.go: add more docs

--- nar/doc.go
+++ nar/doc.go
@@ -1,3 +1,8 @@
-// Package nar implement functions to deal with the Nix Archive format. This
-// is the format that is being used to pack and distribute Nix build results.
+// Package tar implements access to .nar files.
+//
+// Nix Archive (nar) is a file format for storing a directory or a single file
+// in a binary reproducible format. This is the format that is being used to
+// pack and distribute Nix build results. It doesn't store any timestamps or
+// similar fields available in conventional filesystems. .nar files can be read
+// and written in a streaming manner.
 package nar
