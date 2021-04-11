6baa8e76549568923eed2d6854a8bded37a0263a
Florian Klink <flokli@flokli.de>
2021-04-11 18:36:59 +0200

    //nixpath: init
    This adds functions to parse and render nix paths.
    
    Co-Authored-By: edef <edef@edef.eu>

---
+++ nixpath/nixpath.go
@@ -0,0 +1,62 @@
+package nixpath
+
+import (
+	"fmt"
+	"github.com/numtide/go-nix/nixbase32"
+	"path"
+	"regexp"
+	"strings"
+)
+
+const (
+	StoreDir     = "/nix/store"
+	PathHashSize = 20
+)
+
+// middle strips the ^ and $ from a regexp
+func middle(re *regexp.Regexp) string {
+	s := re.String()
+	s = strings.TrimPrefix(s, "^")
+	s = strings.TrimSuffix(s, "$")
+	return regexp.MustCompile(s).String()
+}
+
+var (
+	nameRe = regexp.MustCompile(`^[a-zA-Z0-9+\-_?=][.a-zA-Z0-9+\-_?=]*$`)
+	pathRe = regexp.MustCompile(fmt.Sprintf(
+		`^%v/([0-9a-z]{%d})-(%v)$`,
+		regexp.QuoteMeta(StoreDir),
+		nixbase32.EncodedLen(PathHashSize),
+		middle(nameRe),
+	))
+)
+
+// NixPath represents a nix store path
+type NixPath struct {
+	Name   string
+	Digest []byte
+}
+
+func (n *NixPath) String() string {
+	return path.Join(StoreDir, fmt.Sprintf("%v-%v", nixbase32.EncodeToString(n.Digest), n.Name))
+}
+
+// FromString parses a path string into a nix path,
+// verifying it's syntactically valid
+// It returns an error if it fails to parse
+func FromString(s string) (*NixPath, error) {
+	m := pathRe.FindStringSubmatch(s)
+	if m == nil {
+		return nil, fmt.Errorf("Unable to parse path %v", s)
+	}
+
+	digest, err := nixbase32.DecodeString(m[1])
+	if err != nil {
+		return nil, fmt.Errorf("Unable to decode hash: %v", err)
+	}
+
+	return &NixPath{
+		Name:   m[2],
+		Digest: digest,
+	}, nil
+}
---
+++ nixpath/nixpath_test.go
@@ -0,0 +1,35 @@
+package nixpath
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNixPath(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		exampleNixPathStr := "/nix/store/00bgd045z0d4icpbc2yyz4gx48ak44la-net-tools-1.60_p20170221182432"
+		nixpath, err := FromString(exampleNixPathStr)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, "net-tools-1.60_p20170221182432", nixpath.Name)
+			assert.Equal(t, []byte{
+				0x8a, 0x12, 0x32, 0x15, 0x22, 0xfd, 0x91, 0xef, 0xbd, 0x60, 0xeb, 0xb2, 0x48, 0x1a, 0xf8, 0x85,
+				0x80, 0xf6, 0x16, 0x00,
+			}, nixpath.Digest)
+		}
+
+		// Test to string
+		assert.Equal(t, exampleNixPathStr, nixpath.String())
+	})
+
+	t.Run("invalid hash length", func(t *testing.T) {
+		_, err := FromString("/nix/store/00bgd045z0d4icpbc2yy-net-tools-1.60_p20170221182432")
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid encoding in hash", func(t *testing.T) {
+		_, err := FromString("/nix/store/00bgd045z0d4icpbc2yyz4gx48aku4la-net-tools-1.60_p20170221182432")
+		assert.Error(t, err)
+	})
+}
