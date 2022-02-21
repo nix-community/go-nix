8a84c760453299bc90bf400bd98fbb94b66d597a
Jean-François Roche <jfroche@affinitic.be>
2022-02-21 23:54:18 +0100

    feat: add parser for derivation

---
+++ derivation/derivation.go
@@ -0,0 +1,59 @@
+package derivation
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
+	"github.com/numtide/go-nix/nixpath"
+)
+
+type Derivation struct {
+	_                string            `parser:"@DerivationPrefix"`
+	Outputs          []Output          `json:"outputs" parser:"'[' ((@@ ','?)* @@? )']'','"`
+	InputDerivations []InputDerivation `json:"inputDrvs" parser:"'[' ((@@ ','?)* @@? )?']'','"`
+	InputSources     []string          `json:"inputSrcs" parser:"'[' ((@NixPath ','?)* @NixPath? )?']'','"`
+	Platform         string            `json:"system" parser:"@String ','"`
+	Builder          string            `json:"builder" parser:"@String ','"`
+	Arguments        []string          `json:"args" parser:"'[' ((@String ','?)* (@NixPath|@String)? )?']'','"`
+	EnvVars          []Env             `json:"env" parser:"'[' ((@@ ','?)* (@@)? )']'')'"`
+}
+
+type Output struct {
+	Content       string `json:"name" parser:"'(' @String ','"`
+	Path          string `json:"path" parser:"@NixPath ','"`
+	HashAlgorithm string `json:"hashAlgo" parser:"@String ','"`
+	Hash          string `json:"hash" parser:"@String ')'"`
+}
+
+type InputDerivation struct {
+	Path string   `json:"path" parser:"'(' @NixPath ','"`
+	Name []string `json:"name" parser:"'[' ((@String ','?)* @String? )']' ')'"`
+}
+
+type Env struct {
+	Key   string `parser:"'(' @String ','"`
+	Value string `parser:"(@String|@NixPath)? ')'"`
+}
+
+func ReadDerivation(reader io.Reader) (*Derivation, error) {
+	drv := Derivation{}
+	iniLexer := lexer.MustSimple([]lexer.Rule{
+		{Name: `NixPath`, Pattern: fmt.Sprintf(`"/nix/store/%v"`, nixpath.NameRe.String())},
+		{Name: `DerivationPrefix`, Pattern: `^Derive\(`},
+		{Name: `String`, Pattern: `"(?:\\.|[^"])*"`},
+		{Name: `Delim`, Pattern: `[,()\[\]]`},
+		{Name: "Whitespace", Pattern: `[ \t\n\r]+`},
+	})
+
+	parser := participle.MustBuild(&Derivation{},
+		participle.Lexer(iniLexer),
+		participle.Elide("Whitespace", "DerivationPrefix"),
+		participle.Unquote("NixPath", "String"),
+	)
+
+	err := parser.Parse("", reader, &drv)
+
+	return &drv, err
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/numtide/go-nix
 go 1.15
 
 require (
+	github.com/alecthomas/participle/v2 v2.0.0-alpha7
+	github.com/alecthomas/repr v0.0.0-20200325044227-4184120f674c // indirect
 	github.com/google/go-cmp v0.5.5
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/stretchr/testify v1.6.1
