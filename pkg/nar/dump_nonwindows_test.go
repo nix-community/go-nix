efbe5399db9f4f12345d0ffc52a20c4b55032a0a
Florian Klink <flokli@flokli.de>
2022-06-06 19:31:15 +0200

    pkg/nar: don't run TestDumpPathUnknown on windows
    You can't create fifo files on windows.

---
+++ pkg/nar/dump_nonwindows_test.go
@@ -0,0 +1,32 @@
+//go:build !windows
+// +build !windows
+
+package nar_test
+
+import (
+	"bytes"
+	"path/filepath"
+	"syscall"
+	"testing"
+
+	"github.com/nix-community/go-nix/pkg/nar"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestDumpPathUnknown makes sure calling DumpPath on a path with a fifo
+// doesn't panic, but returns an error.
+func TestDumpPathUnknown(t *testing.T) {
+	tmpDir := t.TempDir()
+	p := filepath.Join(tmpDir, "a")
+
+	err := syscall.Mkfifo(p, 0o644)
+	if err != nil {
+		panic(err)
+	}
+
+	var buf bytes.Buffer
+
+	err = nar.DumpPath(&buf, p)
+	assert.Error(t, err)
+	assert.Containsf(t, err.Error(), "invalid mode", "error should complain about invalid mode")
+}
