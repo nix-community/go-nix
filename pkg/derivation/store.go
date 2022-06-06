220410ded53e36c9da9784b5746027e5a1c6459a
Florian Klink <flokli@flokli.de>
2022-06-06 14:40:03 +0200

    pkg/derivation: add Store interface
    This describes the interface a Derivation store needs to satisfy.

---
+++ pkg/derivation/store.go
@@ -0,0 +1,17 @@
+package derivation
+
+// Store describes the interface a Derivation store needs to implement
+// to be used from here.
+// Note we use pointers to Derivation structs here, so be careful modifying these.
+// Look in the store/ subfolder for implementations.
+type Store interface {
+	// Put inserts a new Derivation into the Derivation Store.
+	// All referred derivation paths should have been Put() before.
+	// The resulting derivation path is returned, or an error.
+	Put(*Derivation) (string, error)
+
+	// Get retrieves a derivation by drv path.
+	// The second return argument specifies if the derivation could be found,
+	// similar to how acessing from a map works.
+	Get(string) (*Derivation, error)
+}
