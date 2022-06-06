a0d935cb69342cc02c277f42477c20a8cadaf15e
Florian Klink <flokli@flokli.de>
2022-06-06 18:38:11 +0200

    pkg/nar: move headerFileInfo.Mode() into header_mode.go
    … also add a header_mode_windows.go file.

--- pkg/nar/header.go
+++ pkg/nar/header.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/fs"
 	"strings"
-	"syscall"
 	"time"
 )
 
@@ -81,27 +80,3 @@ func (fi headerFileInfo) Sys() interface{}   { return fi.h }
 // Name of the file.
 // Will be an empty string, if this describes the root of a NAR.
 func (fi headerFileInfo) Name() string { return fi.h.Path }
-
-func (fi headerFileInfo) Mode() fs.FileMode {
-	// everything in the nix store is readable by user, group and other.
-	var mode fs.FileMode
-
-	switch fi.h.Type {
-	case TypeRegular:
-		mode = syscall.S_IRUSR | syscall.S_IRGRP | syscall.S_IROTH
-		if fi.h.Executable {
-			mode |= (syscall.S_IXUSR | syscall.S_IXGRP | syscall.S_IXOTH)
-		}
-	case TypeDirectory:
-		mode = syscall.S_IRUSR | syscall.S_IRGRP | syscall.S_IROTH
-		mode |= (syscall.S_IXUSR | syscall.S_IXGRP | syscall.S_IXOTH)
-	case TypeSymlink:
-		mode = fs.ModePerm | fs.ModeSymlink
-	case TypeUnknown:
-		// It's not possible to create a NAR with a member of TypeUnknown using either
-		// the reader or the writer, only by manually populating structs.
-		panic("No mode for TypeUnknown")
-	}
-
-	return mode
-}
