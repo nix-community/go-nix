1ef1dc87614fdaeba7d336bd97057a1ebe8c9ee7
Florian Klink <flokli@flokli.de>
2022-06-12 13:19:19 +0200

    //test/testdata: generate fixtures for json outputs, too

--- test/testdata/build_fixtures.sh
+++ test/testdata/build_fixtures.sh
@@ -8,7 +8,11 @@
 # /nix/store/0hm2f1psjpcwg8fijsmr4wwxrx59s092-bar.drv
 bar=$(nix-instantiate derivation_sha256.nix -A bar)
 cp $bar .
+bar_json_path=$(basename $bar).json
+nix show-derivation $bar > $bar_json_path
 
 # /nix/store/4wvvbi4jwn0prsdxb7vs673qa5h9gr7x-foo.drv
 foo=$(nix-instantiate derivation_sha256.nix -A foo)
 cp $foo .
+foo_json_path=$(basename $foo).json
+nix show-derivation $foo > $foo_json_path
