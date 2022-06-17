c969a60698de046c06680d3cadb6530870ff9519
adisbladis <adisbladis@gmail.com>
2022-06-17 20:14:32 +0800

    pkg/derivation: Add unicode test

--- pkg/derivation/derivation_test.go
+++ pkg/derivation/derivation_test.go
@@ -143,6 +143,10 @@ func TestEncoder(t *testing.T) {
 			Title:          "structured-attrs",
 			DerivationFile: "9lj1lkjm2ag622mh4h9rpy6j607an8g2-structured-attrs.drv",
 		},
+		{
+			Title:          "unicode",
+			DerivationFile: "52a9id8hx688hvlnz4d1n25ml1jdykz0-unicode.drv",
+		},
 	}
 
 	t.Run("WriteDerivation", func(t *testing.T) {
@@ -381,6 +385,10 @@ func TestDrvPath(t *testing.T) {
 		{
 			Title:          "structured-attrs",
 			DerivationFile: "9lj1lkjm2ag622mh4h9rpy6j607an8g2-structured-attrs.drv",
+		},
+		{
+			Title:          "unicode",
+			DerivationFile: "52a9id8hx688hvlnz4d1n25ml1jdykz0-unicode.drv",
 		},
 	}
 
---
+++ test/testdata/unicode.nix
@@ -0,0 +1,13 @@
+builtins.derivation {
+  name = "unicode";
+  builder = ":";
+  system = ":";
+  letters = ''
+    räksmörgås
+    rødgrød med fløde
+    Lübeck
+    肥猪
+    こんにちは / 今日は
+    🌮
+  '';
+}
