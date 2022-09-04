f7328b2681ce2526412937e73bce1790d2d568d5
Florian Klink <flokli@flokli.de>
2022-09-04 15:57:28 +0200

    pkg/exp/store: add Tree and Blob structs
    … as well as an Entry struct, which refers to other objects (by their
    digest).

---
+++ pkg/exp/store/types.go
@@ -0,0 +1,25 @@
+package store
+
+type EntryMode string
+
+const (
+	TypeFileRegular    = EntryMode("100644")
+	TypeFileExecutable = EntryMode("100755")
+	TypeSymlink        = EntryMode("120000")
+	TypeDirectory      = EntryMode("40000")
+	// omitted: submodule mode
+)
+
+type Entry struct {
+	ID   []byte
+	Mode EntryMode
+	Name string
+}
+
+type Tree struct {
+	Entries []*Entry
+}
+
+type Blob struct {
+	Contents []byte
+}
