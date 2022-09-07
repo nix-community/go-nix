1893d2fe3cc4677b07fc908dfb0cf4dd96d82778
Florian Klink <flokli@flokli.de>
2022-09-07 02:05:55 +0200

    nix: init basic nix plumbing
    Provide some devtools needed to interact with the repository.

---
+++ nix/default.nix
@@ -0,0 +1,33 @@
+let
+  sources = (import ./sources.nix);
+  pkgs = import sources.nixpkgs { };
+
+  profileEnv = pkgs.writeTextFile {
+    name = "profile-env";
+    destination = "/.profile";
+    # This gets sourced by direnv. Set NIX_PATH, so `nix-shell` uses the same nixpkgs as here.
+    text = ''
+      export NIX_PATH=nixpkgs=${toString pkgs.path}
+    '';
+  };
+  env = pkgs.buildEnv {
+    name = "dev-env";
+    paths = [
+      profileEnv
+
+      pkgs.niv
+
+      pkgs.buf
+      pkgs.go_1_19
+      pkgs.protobuf
+      #pkgs.protoc-gen-connect-go
+      pkgs.protoc-gen-go
+
+      pkgs.gofumpt
+      pkgs.golangci-lint
+    ];
+  };
+in
+{
+  inherit env pkgs;
+}
